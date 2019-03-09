6092f7a7abdcc762d26c7a8074d2ef4232dca1aa
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-09 21:52:10 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,11 +3,11 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    self.songs.count
+    self.songs.size
   end
 
   def artist_count
-    self.artists.count
+    self.artists.size
   end
 
   def all_artist_names
